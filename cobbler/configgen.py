dad81298d50b304c1636968c76876b031977cb4d
SchoolGuy <matrixfueller@gmail.com>
2021-05-30 15:53:05 +0200

    Fix the CLI and some other things

--- cobbler/configgen.py
+++ cobbler/configgen.py
@@ -152,7 +152,7 @@ class ConfigGen:
                     t = template_api.CobblerTemplate(file=file_data[file]['template'], searchList=[self.host_vars])
                     file_data[file]['content'] = t.respond()
                 except:
-                    utils.die(self.logger, "Missing template for this file resource %s" % (file_data[file]))
+                    utils.die("Missing template for this file resource %s" % (file_data[file]))
 
         config_data['files'] = file_data
         return config_data
